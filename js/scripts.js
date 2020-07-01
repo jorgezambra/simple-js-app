8299b3a660b18bbbd2dfbc8f8ea9a765e431a359
jorgezambra <jorgezambra@gmail.com>
2020-07-02 01:31:31 +0200

    task 1.7
    updated and added functions to load data from external source.   added loadlist, loadetails

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,72 @@
 var pokemonRepository = (function () {
-  var pokemonList = [
-{
-  name: 'Bulbasaur',
-  height: 0.7,
-  types: ['grass', 'poison'],
-},
-{
-  name: 'Charmander',
-  height: 0.6,
-  types: ['fire'],
-},
-{
-  name: 'Charizard',
-  height: 1.7,
-  types: ['fire', 'flying'],
-}
-];
+  var repository = [];
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-function add(pokemon) {
-    pokemonList.push(pokemon);
+  function add(pokemon) {
+      repository.push(pokemon);
+    }
+
+  function getAll() {
+      return repository;
+    }
+
+  function addListItem(pokemon) {
+    var pokemonList = document.querySelector('ul');
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-name');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    button.addEventListener('click', function (event) {showDetails(pokemon)
+    });
   }
 
-function getAll() {
-    return pokemonList;
+  function loadList() {
+      return fetch(apiUrl).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        json.results.forEach(function (item) {
+          var pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      }).catch(function (e) {
+        console.error(e);
+      })
+    }
+
+    function loadDetails(item) {
+        var url = item.detailsUrl;
+        return fetch(url).then(function(response) {
+          return response.json();
+        }).then(function (details) {
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+
+    function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
   }
-
-function addListItem(pokemon) {
-  var pokemonList = document.querySelector('ul');
-  var listItem = document.createElement('li');
-  var button = document.createElement('button');
-  button.innerText = pokemon.name;
-  button.classList.add('pokemon-name');
-  listItem.appendChild(button);
-  pokemonList.appendChild(listItem);
-  button.addEventListener('click', function (event) {showDetails(pokemon.name)
-  });
-}
-
-function showDetails(pokemon) {
-  console.log(pokemon)
-}
-
 return {
     add: add,
     getAll: getAll,
-    addListItem : addListItem
+    addListItem : addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
 })();
 
-pokemonRepository.add({
-  name : 'Wartortle',
-  height : 3.03,
-  types: ['Water'],
-});
-
-pokemonRepository.getAll().forEach(function (pokemon){
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon){
     pokemonRepository.addListItem(pokemon);
-  })
+  });
+});
